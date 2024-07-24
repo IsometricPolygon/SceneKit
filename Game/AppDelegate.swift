f52feb054e85237bbfb32de0e9c1da851a20c6bf
IsometricPolygon <holdenjthompson@icloud.com>
2024-07-23 21:01:49 -0400

    Initial Commit

---
+++ Game/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Game
+//
+//  Created by Holden Thompson on 7/23/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
